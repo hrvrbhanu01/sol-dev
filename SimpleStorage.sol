632944559a7047efc1b0d0658908064c3f85575f
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2024-08-20 12:02:00 +0530

    day4 : commit

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,26 +3,38 @@ pragma solidity ^0.8.18;
 
 contract testSimpleStorage {
     //Basic Types: boolean, uint, int, address, bytes
-    //uint == uint256 
+    //uint == uint256
     //int == int256
     //int; could be positive or negative but uint; will always be positive
     //so the default value for : uint favoriteNumber will be 0, also for the boolean type it is false.
     //FOR MORE NOTES REFER TO THE NOTES DIARY.
-    uint256 public favoriteNumber = 80;
-    uint256 newFavoriteNumber; //0
+    uint256 myFavoriteNumber; //0
+
+    //uint256[] listOfFavoriteNumbers;
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople; //[]  empty list
+
+    //Person public pat = Person({favoriteNumber: 7, name: "pat"});
 
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
-        retrieve(); //view functions don't cost any gas fees until unless you are not using them within some 
+        myFavoriteNumber = _favoriteNumber;
+        retrieve(); //view functions don't cost any gas fees until unless you are not using them within some
         //function which is a gas costing function ; like this one here: store()
     }
 
     //view, pure functions:
     //view: just reads from the blockchain
     //pure: writes as well onto the blockchain
-    function retrieve() public view returns(uint256){
-        return favoriteNumber;
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
     }
 
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        //Person memory newPerson = Person(_favoriteNumber, _name);
+        listOfPeople.push(Person(_favoriteNumber, _name));
+    }
 }
-
