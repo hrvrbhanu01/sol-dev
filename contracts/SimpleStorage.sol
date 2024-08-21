e93a341cddac2bafeb09a46535503db37696c528
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2024-08-21 15:10:16 +0530

    calling contracts through contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.18;
 
 contract SimpleStorage {
     //Basic Types: boolean, uint, int, address, bytes
@@ -43,3 +43,7 @@ contract SimpleStorage {
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
+
+contract SimpleStorage2 {}
+
+contract SimpleStorage3 {}
