616f507e7017633234ef4e7ca0b023fce57f4533
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2024-08-22 11:48:10 +0530

    deployed contract test net

--- FundMe.sol
+++ FundMe.sol
@@ -6,8 +6,13 @@
 
 pragma solidity ^0.8.18;
 
-contract FundMe{
-    function fund() public {
-        
+contract FundMe {
+    function fund() public payable {
+        // allow users to send $
+        // Have a minimum $ sent
+        // 1. How do we send ETH to this contract?
+        require(msg.value > 1e18, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 Wei = 1 * 10 ** 18
     }
+
+    //function withdraw() public {}
 }
