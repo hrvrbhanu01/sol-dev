4e7fd2276f7e29e07abc1f80eeb24d318e4668dd
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2024-08-22 15:06:48 +0530

    getting price in USD

--- FundMe.sol
+++ FundMe.sol
@@ -6,13 +6,35 @@
 
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol"; // this is for us getting ABI value.
+
 contract FundMe {
+
+    uint256 minimumUsd = 5;
+
     function fund() public payable {
         // allow users to send $
         // Have a minimum $ sent
         // 1. How do we send ETH to this contract?
-        require(msg.value > 1e18, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 Wei = 1 * 10 ** 18
+        require(msg.value >= minimumUsd, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 Wei = 1 * 10 ** 18
     }
 
     //function withdraw() public {}
+
+    function getPrice() public view returns (uint256) {
+        // Address  0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        // Price of ETH in terms of USD
+        // 200.00000000
+        return uint256(price * 1e10);
+        
+    }
+    function getConversionRate() public {}
+
+    function getVersion() public view returns (uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
+
 }
