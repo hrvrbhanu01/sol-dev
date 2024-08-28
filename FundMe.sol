1581a8a61655f060a9db04ea5979cf7baa7dd124
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2024-08-28 14:50:57 +0530

    PriceConverter.sol library added!

--- FundMe.sol
+++ FundMe.sol
@@ -6,9 +6,10 @@
 
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol"; // this is for us getting ABI value.
+import { PriceConverter } from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;  // all uint256 values can now call getConversionRate() from PriceConverter.sol
 
     uint256 public minimumUsd = 5 * 1e18;
 
@@ -16,37 +17,10 @@ contract FundMe {
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
     function fund() public payable {
-        // allow users to send $
-        // Have a minimum $ sent
-        // 1. How do we send ETH to this contract?
-        require(getConversionRate(msg.value) >= minimumUsd, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 Wei = 1 * 10 ** 18
+        require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 Wei = 1 * 10 ** 18
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
     //function withdraw() public {}
-
-    function getPrice() public view returns (uint256) {
-        // Address  0x694AA1769357215DE4FAC081bf1f309aDC325306
-        // ABI
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 price,,,) = priceFeed.latestRoundData();
-        // Price of ETH in terms of USD
-        // 200.00000000
-        return uint256(price * 1e10);
-        
-    }
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        // 1 ETH?
-        // 2000_000000000000000000
-        uint256 ethPrice = getPrice();
-        // (2000_000000000000000000 * 1_000000000000000000) / 1e18
-        // $2000 = 1 ETH
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; // in solidity you will want to multiply before you divide.
-        return ethAmountInUsd;
-    }
-
-    function getVersion() public view returns (uint256){
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-    }
 
 }
