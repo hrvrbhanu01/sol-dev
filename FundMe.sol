9729008c96c318d0d9aa0f8ae6ec6d66c0df18fc
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2024-08-29 11:35:44 +0530

    using .call method for withdraw ETH

--- FundMe.sol
+++ FundMe.sol
@@ -18,9 +18,39 @@ contract FundMe {
 
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 Wei = 1 * 10 ** 18
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    //function withdraw() public {}
+    function withdraw() public {
+        // for loop
+        // [1, 2, 3, 4] elements
+        // 0, 1, 2, 3   indexes
+        //for( starting index, ending index, step amount )
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+            
+        }
+        //reset the funders array
+        funders = new address[](0);
+        // withdraw the funds :
+
+        // 1.transfer 
+        // msg.sender = address
+        // payable(msg.sender) = payable address
+
+        /* payable(msg.sender).transfer(address(this).balance); */
+
+        // 2.send 
+
+        /* bool sendSuccess = payable(msg.sender).send(address(this).balance);  // refer doc at : solidity-by-example.org/sending-ether
+        require(sendSuccess, "Send Failed!"); */
+
+        // 3.call
+
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed!");
+
+    }
 
 }
